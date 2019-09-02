3d631b394b1cfa397a879dbbafba8ae03acba422
miaowenting <wenting.mwt@dtwave-inc.com>
2019-09-02 19:10:36 +0800

    modify build.sh 、docker-compose.yml and so on

--- README.md
+++ README.md
@@ -43,3 +43,51 @@ GROUP BY
 ----
 
 *Apache Flink, Flink®, Apache®, the squirrel logo, and the Apache feather logo are either registered trademarks or trademarks of The Apache Software Foundation.*
+
+## docker compose 技术
+
+docker-compose.yml配置文件中的image中都可以从Data Hub: [https://hub.docker.com] 中搜索下载
+
+flink-sql-client-training:
+[https://hub.docker.com/r/fhueske/flink-sql-client-training-1.7.2/tags]
+
+flink:
+[https://hub.docker.com/_/flink?tab=tags]
+
+zookeeper:
+[https://hub.docker.com/r/wurstmeister/zookeeper/tags]
+
+kafka:
+[https://hub.docker.com/r/wurstmeister/kafka/tags]
+
+elasticsearch:
+[https://hub.docker.com/r/elastic/elasticsearch/tags]
+
+compose文件是一个定义服务、网络、卷的yaml文件，默认路径是./docker-compose.yml，扩展名可以为yml或yaml
+
+- depends_on
+  解决了启动顺序问题
+  
+- environment
+  添加环境变量
+
+- hostname
+
+- expose
+  暴露端口，但不映射到宿主机，只被连接的服务访问。
+  
+- ports
+  映射端口
+  
+- command
+  覆盖容器启动后默认执行的命令
+
+- links
+  解决的是容器连接问题，会连接到其他服务中的容器
+  
+- volumes
+  挂在一个目录或一个已存在的数据卷容器，可以有效保护宿主机的文件系统
+
+- ulimits
+  覆盖容器的默认限制
+
--- build-image/build.sh
+++ build-image/build.sh
@@ -9,6 +9,10 @@ HERE
   exit 1
 }
 
+# 构建fhueske/flink-sql-client-training-1.7.2镜像
+# 这个已经有编译好的镜像上传到Docker Hub上了，可以从Docker Hub中直接下载镜像:
+# https://hub.docker.com/r/fhueske/flink-sql-client-training-1.7.2/tags/latest
+
 USER_NAME="fhueske"
 IMAGE_NAME="flink-sql-client-training-1.7.2"
 IMAGE_VERSION=`cat VERSION`
@@ -32,14 +36,22 @@ mvn -f sql-udfs clean install
 
 # create docker file
 echo -e "FROM ${USER_NAME}/${BASE_IMAGE_NAME}:${BASE_IMAGE_VERSION}\n" > Dockerfile.tmp
+
 # copy SQL Client config
 echo -e "COPY training-config.yaml /opt/sql-client/conf/config.yaml\n" >> Dockerfile.tmp
+
 # copy UDFs
 echo -e "COPY sql-udfs/target/sql-udfs-1.0-SNAPSHOT.jar /opt/sql-client/lib/\n" >> Dockerfile.tmp
-# download data files
-echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${DRIVERS_DATA_FILE}' '${DRIVERS_DATA_URL}'\n" >> Dockerfile.tmp
-echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${FARES_DATA_FILE}' '${FARES_DATA_URL}'\n" >> Dockerfile.tmp
-echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${RIDES_DATA_FILE}' '${RIDES_DATA_URL}'\n" >> Dockerfile.tmp
+
+# download data files, but drive.google.com Connection refused
+# echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${DRIVERS_DATA_FILE}' '${DRIVERS_DATA_URL}'\n" >> Dockerfile.tmp
+# echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${FARES_DATA_FILE}' '${FARES_DATA_URL}'\n" >> Dockerfile.tmp
+# echo -e "RUN wget -O '${KAFKA_DATA_PATH}/${RIDES_DATA_FILE}' '${RIDES_DATA_URL}'\n" >> Dockerfile.tmp
+
+# copy data files
+echo -e "COPY ${DRIVERS_DATA_FILE} ${KAFKA_DATA_PATH}/${DRIVERS_DATA_FILE}\n" >> Dockerfile.tmp
+echo -e "COPY ${FARES_DATA_FILE} ${KAFKA_DATA_PATH}/${FARES_DATA_FILE}\n" >> Dockerfile.tmp
+echo -e "COPY ${RIDES_DATA_FILE} ${KAFKA_DATA_PATH}/${RIDES_DATA_FILE}\n" >> Dockerfile.tmp
 
 # build image
 docker build -t "${USER_NAME}/${IMAGE_NAME}:${IMAGE_VERSION}" -f ./Dockerfile.tmp .
